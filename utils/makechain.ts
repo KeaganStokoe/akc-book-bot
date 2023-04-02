1c16907090458bdcdf4ca7cf646d8c96e20e306b
Keagan Stokoe <keagan.stokoe@gmail.com>
2023-04-02 16:48:07 +0200

    increase sources to 2

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -60,6 +60,6 @@ export const makeChain = (
     combineDocumentsChain: docChain,
     questionGeneratorChain: questionGenerator,
     returnSourceDocuments: true,
-    k: 1, //number of source documents to return
+    k: 2, //number of source documents to return
   });
 };
